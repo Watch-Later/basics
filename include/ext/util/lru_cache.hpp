8cd1d87c6a7a3dd5fa3eff85fe25706d1c811e9e
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-09-10 22:18:48 +0200

    WIP: Add some tests and change predicates to template type from std::function.

--- include/ext/util/lru_cache.hpp
+++ include/ext/util/lru_cache.hpp
@@ -14,83 +14,137 @@ namespace ext::util {
 template <typename Key, typename Value>
 class lru_cache {
 public:
-	lru_cache(size_t max_size) : _max_size{max_size} { }
+    lru_cache(size_t max_size) : _max_size{max_size} { }
     lru_cache(lru_cache const&) = delete;
 
-	void put(const Key& key, const Value& value) {
-		std::lock_guard<std::mutex> guard(_mut);
-		auto found = _map.find(key);
+    static void default_update(Value&){};
+    static bool default_remove(Value const&){ return false; };
 
-	    if (found == _map.end()) {
-	        if (_map.size() + 1 > _max_size) {
-	            auto last = _list.crbegin();
-	            _map.erase(last->first);
-	            _list.pop_back();
-	        }
+    void put(const Key& key, const Value& value) {
+        std::lock_guard<std::mutex> guard(_mut);
+        auto found = _map.find(key);
 
-	        _list.emplace_front(key, value);
-	        _map[key] = _list.begin();
-	    } else {
-	        found->second->second = value;
-	        _list.splice(_list.cbegin(), _list, found->second);
-	    }
-	}
+        if (found == _map.end()) {
+            if (_map.size() + 1 > _max_size) {
+                auto last = _list.crbegin();
+                _map.erase(last->first);
+                _list.pop_back();
+            }
 
-	const std::pair<Value, bool> get(const Key& key, std::function<void(Value&)> update) {
-	std::lock_guard<std::mutex> guard(_mut);
-	    auto found = _map.find(key);
-	    if (found == _map.end()) {
-	        return {Value{}, false};
-	    } else {
-	        update(found->second->second);
-	        _list.splice(_list.begin(), _list, found->second);
-	        return {found->second->second, true};
-	    }
-	}
+            _list.emplace_front(key, value);
+            _map[key] = _list.begin();
+        } else {
+            found->second->second = value;
+            _list.splice(_list.cbegin(), _list, found->second);
+        }
+    }
 
-	const std::pair<Value, bool> get(const Key& key) {
-	std::lock_guard<std::mutex> guard(_mut);
-	    auto found = _map.find(key);
-	    if (found == _map.end()) {
-	        return {Value{}, false};
-	    } else {
-	        _list.splice(_list.begin(), _list, found->second);
-	        return {found->second->second, true};
-	    }
-	}
+    template<typename UpdatePredicate = decltype(default_update), typename RemovePredicate = decltype(default_remove)>
+    const std::pair<Value, bool> get(const Key& key, UpdatePredicate update_pred = &default_update, RemovePredicate remove_pred = &default_remove) {
+        static_assert(std::is_convertible_v<UpdatePredicate, std::function<void(Value&)>>, "Wrong Singnature for UpdatePredicate");
+        static_assert(std::is_convertible_v<RemovePredicate, std::function<bool(Value&)>>, "Wrong Singnature for RemovePredicate");
 
-	bool exists(const Key& key) const noexcept {
-		std::lock_guard<std::mutex> guard(_mut);
-	    return _map.find(key) != _map.end();
-	}
+        std::lock_guard<std::mutex> guard(_mut);
 
-	bool update_if_exists(const Key& key, std::function<void(Value&)> update) {
-		std::lock_guard<std::mutex> guard(_mut);
-		auto found = _map.find(key);
-		if (found != _map.end()) {
-			update(found->second->second);
-			return true;
-		} else {
-			return false;
-		}
+        auto found = _map.find(key);
+        if (found == _map.end()) {
+            return {Value{}, false};
+        } else {
+            Value& value = found->second->second;
+            if (remove_pred(value)) {
+                // move value to end of list to end of list
+                _list.splice(_list.cend(), _list, found->second);
+                //remove item from map
+                _map.erase(found);
+                //delete element
+                _list.pop_back();
+                return {Value{}, false};
+            }
+            update_pred(value);
+            _list.splice(_list.begin(), _list, found->second);
+            return {value, true};
+        }
+    }
 
-	}
+    bool exists(const Key& key) const noexcept {
+        std::lock_guard<std::mutex> guard(_mut);
 
-	size_t size() const noexcept {
-	    std::lock_guard<std::mutex> guard(_mut);
-	    return _map.size();
-	}
+        return _map.find(key) != _map.end();
+    }
 
-    // fast clean up of expired values
-    //void remove_all_matching_predicate(...);
-    //void remove_past_value_matching_predicate(...);
+    template<typename UpdatePredicate = decltype(default_update), typename RemovePredicate = decltype(default_remove)>
+    bool exists(const Key& key, UpdatePredicate update_pred = default_update, RemovePredicate remove_pred = default_remove) {
+        static_assert(std::is_convertible_v<UpdatePredicate, std::function<void(Value&)>>, "Wrong Singnature for UpdatePredicate");
+        static_assert(std::is_convertible_v<RemovePredicate, std::function<bool(Value&)>>, "Wrong Singnature for RemovePredicate");
+        std::lock_guard<std::mutex> guard(_mut);
+
+        auto found = _map.find(key);
+        if (found != _map.end()) {
+            Value& value = found->second->second;
+            if (remove_pred(value)) {
+                // move value to end of list to end of list
+                _list.splice(_list.cend(), _list, found->second);
+                //remove item from map
+                _map.erase(found);
+                //delete element
+                _list.pop_back();
+                return false;
+            }
+            update_pred(value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool remove(const Key& key) {
+        std::lock_guard<std::mutex> guard(_mut);
+
+        auto found = _map.find(key);
+        if (found != _map.end()) {
+            // move value to end of list to end of list
+            _list.splice(_list.cend(), _list, found->second);
+            //remove item from map
+            _map.erase(found);
+            //delete element
+            _list.pop_back();
+            return true;
+        }
+        return false;
+    }
+
+
+    template<typename RemovePredicate, typename = std::enable_if_t<!std::is_same_v<RemovePredicate,Value>>>
+    std::size_t remove(RemovePredicate remove_pred) {
+        static_assert(std::is_convertible_v<RemovePredicate, std::function<bool(Value&)>>, "Wrong Singnature for RemovePredicate");
+        std::lock_guard<std::mutex> guard(_mut);
+
+        std::size_t n = 0;
+        for (auto it = _list.begin(); it != _list.end();) {
+            Value& value = it->second;
+            if (remove_pred(value)) {
+                _map.erase(value);
+                ++it;
+                _list.pop_front();
+                ++n;
+            } else {
+                ++it;
+            }
+        }
+        return n;
+    }
+
+    size_t size() const noexcept {
+        std::lock_guard<std::mutex> guard(_mut);
+        return _map.size();
+    }
 
 private:
-	std::list<std::pair<Key, Value>> _list;
-	std::unordered_map<Key, decltype(_list.begin())> _map;
-	size_t _max_size;
-	mutable std::mutex _mut;
+    std::list<std::pair<Key, Value>> _list;
+    std::unordered_map<Key, decltype(_list.begin())> _map;
+    size_t _max_size;
+    mutable std::mutex _mut;
 };
 
-}     // namespace ext::util
+} // namespace ext::util
 #endif // EXT_UTIL_LRU_CACHE_HEADER
